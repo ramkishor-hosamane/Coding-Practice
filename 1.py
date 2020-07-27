4b969fd263c0b68654245bff499d4e2f9735d466
Code-With-Ram <ramhosamane@gmail.com>
2020-07-27 11:52:00 +0530

    1

--- 1.py
+++ 1.py
@@ -4,15 +4,19 @@ Find the largest palindrome in a given string.
 def is_palindrome(s):
 	return s[:]==s[::-1]
 
-def largest_palindrome(s,j,i=0):
 
-	if is_palindrome(s[i:j]):
-		return s[i:j]
-	else:
-		return max(largest_palindrome(s,j-1,i),largest_palindrome(s,j,i+1),key=len)
+def larget_palindrome(s):
+        res=""
+        for word in s:
+                if is_palindrome(word):
+                      res=max(res,word,key=len)
 
-
+        if res="":
+                print("There are no palindromes")
+        else:
+                print(res,"is the largest palindrome")
+        
 s = "aabba"
-s = "aabbaaa"
+s = "saas mmaa iti Malayalam "
 
 print(largest_palindrome(s,len(s)))
--- 4.py
+++ 4.py
@@ -3,29 +3,21 @@
 exactly one is repeated find the repeated and missing number.
 '''
 
-#solution 1
-def find_duplicate1(arr,n):
-	Sum =  n * (n+1)/2
-	return sum(arr) - Sum
-
-#solution 2
-def find_duplicate2(arr,n):
-	arr.sort()
-	for i in range(1,n):
-		if arr[i]==arr[i-1]:
-			return arr[i]
-
 
 #solution 3
-def find_duplicate3(arr,n):
-	h = {}
+def find_duplicate(arr,n):
+	h = {x:0 for x in range(1,n+1,1)}
 	for x in arr:
-		if h.get(x):
-			return x
-		else:
-			h[x] = x
+		h[x]+=1
+	#print(h)
 
+	for x in h:
+		if h[x] == 2:
+			print(x,"is repeated Twice")
+			
+		if h[x] == 0:
+			print(x,"is the missing number")
 
-a = [3,4,5,2,6,1,5]
-print(find_duplicate3(a,6))
+a = [1,2,3,3]
+find_duplicate(a,len(a))
 
